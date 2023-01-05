04a735dc9235603a399b28aa72a815b5f18365d1
radityohanif <radityo.hanif@gmail.com>
2023-01-05 23:32:48 +0700

    Initial commit

---
+++ app.vue
@@ -0,0 +1,36 @@
+<template>
+  <Navbar />
+  <div class="hero min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Hello Gais</h1>
+        <p class="py-6">Daisy UI Amazing 😍</p>
+        <button class="btn btn-primary">Learn Now</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="hero min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold mb-5">Card Components</h1>
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <figure class="px-10 pt-10">
+            <img
+              src="https://placeimg.com/400/225/arch"
+              alt="Shoes"
+              class="rounded-xl"
+            />
+          </figure>
+          <div class="card-body items-center text-center">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions">
+              <button class="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
